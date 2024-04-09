0cd62fcfe496ff1b1791687eaa1422826433a77c
mohanataraj <mohan1519@gmail.com>
2024-04-08 17:09:43 -0700

    added mobile menu & padding to contents

--- src/app/components/navigation/page.tsx
+++ src/app/components/navigation/page.tsx
@@ -14,6 +14,7 @@ export default function NavigationHeader (){
     const [open, setOpen] = useState(false);
  
     const handleOpen = () => setOpen(!open);
+    const [isOpen, setIsOpen] = useState(false);
 
 
 
@@ -22,19 +23,47 @@ export default function NavigationHeader (){
 <header className="absolute z-50 w-full left-0 top-0">
 
     <div className="container"> 
-        <nav className="bg-white py-2.5 dark:bg-white">
+        <nav className="bg-white justify-between flex-wrap py-3 dark:bg-white">
             <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-                <a href="#" className="flex items-center px-4">
+                <a href="/" className="flex items-center">
                     <img src="/logo_temp.svg" className="h-6 mr-3 sm:h-9" alt="Live Logo" />
                     <span className="self-center italics font-mono text-black text-xl font-semibold whitespace-nowrap dark:text-black">LIVE</span>
                 </a>
-                <div className="flex flex-row md:flex-shrink items-center lg:order-2">
-                
-                <Link href="/about" className="p-1">About</Link>
-                <Link href="/feedback" className="p-1">Feedback</Link>
-                <Link href="/contact" className="p-1">Contact</Link>
-                <Link href="https://www.figma.com/proto/t0Ox1a9UnJU4VKUQkWK846/Live-auto-insurance-prototype?page-id=0%3A1&type=design&node-id=1-3&viewport=114%2C125%2C0.16&t=G08BDM6VndWg33tb-1&scaling=scale-down&starting-point-node-id=1%3A3&mode=design" target="_blank" rel="noopener noreferrer" className="p-1">Prototype</Link>
-
+     <div className="block lg:hidden">
+       <button
+         onClick={() => setIsOpen(!isOpen)}
+         className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
+       >
+         <svg
+           className={`fill-current h-3 w-3 ${isOpen ? "hidden" : "block"}`}
+           viewBox="0 0 20 20"
+           xmlns="http://www.w3.org/2000/svg"
+         >
+           <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+         </svg>
+         <svg
+           className={`fill-current h-3 w-3 ${isOpen ? "block" : "hidden"}`}
+           viewBox="0 0 20 20"
+           xmlns="http://www.w3.org/2000/svg"
+         >
+           <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+         </svg>
+       </button>
+     </div>
+     <div
+       className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? "block" : "hidden"}`}
+     >
+                    <div className="flex md:flex-shrink items-center lg:order-2">
+                        
+                        <Link href="/about" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4">About</Link>
+                        
+                        <Link href="/feedback" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4">Feedback</Link>
+                        
+                        <Link href="/contact" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4">Contact</Link>
+                        
+                        <Link href="https://www.figma.com/proto/t0Ox1a9UnJU4VKUQkWK846/Live-auto-insurance-prototype?page-id=0%3A1&type=design&node-id=1-3&viewport=114%2C125%2C0.16&t=G08BDM6VndWg33tb-1&scaling=scale-down&starting-point-node-id=1%3A3&mode=design" target="_blank" rel="noopener noreferrer" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4">Prototype</Link>
+                        
+                    </div>
             </div>
         </div>
     </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,14 +20,14 @@ export default function Home (){
         <div className="grid max-w-screen-xl px-4  pb-8 sm:grid-cols-1 sm:mt-3 md:grid-cols-1 md:m-10 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 ">
             <div className="mr-auto lg:col-span-7">
                 <h1 className="max-w-2xl mb-4 text-4xl font-extrabold bg-gradient-to-r from-[#0ea5e9] to-[#2563eb] inline-block text-transparent bg-clip-text leading-none tracking-tight  md:p-5 md:text-5xl xl:text-6xl dark:text-blue-700">We drive with <br />you & your family.</h1>
-                <p className="max-w-2xl mb-6 font-regular text-blue-gray-800 md:p-5 lg:mb-8 md:text-lg lg:text-lg dark:text-zinc-700"> We at Live are building next generation auto insurance and live traffic view infrastructure. <br /> &emsp; 
-                With our computer vision based A.I. models we have quicker evidence based automated claim process and real-time updates for maps.
+                <p className="max-w-2xl mb-6 font-regular text-blue-gray-800 md:p-5 lg:mb-8 md:text-lg lg:text-lg dark:text-zinc-700"> We at Live are building next generation auto insurance and real-time traffic view infrastructure that updates maps in real-time. <br /> &emsp; 
+                Join us on our journey.
                 </p>
                 
                 <SignupForm />
               
             </div>
-            <div className="overflow-auto sm:overflow-visible  sm:pl-3 lg:mt-0 lg:col-span-5 lg:flex">
+            <div className="overflow-auto sm:overflow-visible mt-3  sm:pl-3 lg:mt-0 lg:col-span-5 lg:flex">
               <video className="h-full w-full rounded-lg aspectVideo" controls autoPlay>
                  <source src ="/videos/live_vid.mp4" type="video/mp4" />
                 Your browser does not support the video tag.

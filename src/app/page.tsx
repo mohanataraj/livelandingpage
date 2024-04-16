4696807d451e33211a06ffb90b0573bc442bd017
mohanataraj <mohan1519@gmail.com>
2024-04-16 00:46:41 -0700

    Edited text due to grammar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,12 +20,13 @@ export default function Home (){
         <div className="grid max-w-screen-xl px-4  pb-8 sm:grid-cols-1 sm:mt-3 md:grid-cols-1 md:m-10 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 ">
             <div className="mr-auto lg:col-span-7">
                 <h1 className="max-w-2xl mb-4 text-4xl font-bold bg-gradient-to-r from-[#000000] to-[#000000] inline-block text-transparent bg-clip-text leading-none tracking-tight  md:p-5 md:text-5xl xl:text-6xl dark:text-blue-700"> 
-               High auto insurance & stressed like Anna?
+                High premium and claim issues? 
                 </h1>
-                <p className="max-w-2xl mb-6 font-regular text-[#424242] md:p-5 lg:mb-8 md:text-lg lg:text-lg dark:text-zinc-700"> 
-                  <span className="text-[#3f3e3e] font-semibold text-xl">We are building future of auto insurance & more. </span> <br/> 
-                  We provide low premium for your auto insurance irrespective of your age, create your own policies and one tap claim process.
-                  <br/> <br/><span className="text-[#3f3e3e] font-semibold"> Leave stress behind, take your ride with confidence. </span>
+                <p className="max-w-2xl mb-6 font-regular text-[#3f3e3e] md:p-5 lg:mb-8 md:text-lg lg:text-lg dark:text-zinc-700"> 
+                 
+                  We provide low premiums for auto insurance, a quick and easy way to create your own policy tailored to your needs and lifestyle
+                  and one tap claim process with no agents involved.
+                  <br/> <br/><span className="text-[#3f3e3e] font-regular"> Leave your stress behind and take your ride with confidence. </span>
                 </p>
                 
                 <SignupForm />
